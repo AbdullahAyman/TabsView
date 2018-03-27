38b454f700cc7d1b8ccfcb6a5c931ecf67ad5e00
Abdullah Ayman <aayman@victorylink.com>
2018-03-27 16:52:36 +0200

    new project

---
+++ app/src/main/java/com/victorylink/tabsbarview/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.victorylink.tabsbarview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aayman on 3/27/2018.
+ */
+
+public class HomeFragment extends Fragment {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
